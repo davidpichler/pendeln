df24c35f589a47068ba65596008156128a0d5e45
David Pichler <pichler.david@hotmail.de>
2025-04-05 18:53:07 +0200

    debug2

--- codes/refresh_token.sh
+++ codes/refresh_token.sh
@@ -5,12 +5,19 @@ echo "APP_KEY: $APP_KEY"
 echo "APP_SECRET: (versteckt)"
 echo "REFRESH_TOKEN: ${REFRESH_TOKEN:0:6}..."
 
-ACCESS_TOKEN=$(curl -s -X POST https://api.dropboxapi.com/oauth2/token \
+# API-Call und Antwort speichern
+RESPONSE=$(curl -s -X POST https://api.dropboxapi.com/oauth2/token \
   -u "$APP_KEY:$APP_SECRET" \
   -d grant_type=refresh_token \
-  -d refresh_token=$REFRESH_TOKEN | jq -r '.access_token')
+  -d refresh_token=$REFRESH_TOKEN)
 
+# Debug-Ausgabe der ganzen Antwort
+echo "Dropbox API Antwort:"
+echo "$RESPONSE"
+
+# Token extrahieren
+ACCESS_TOKEN=$(echo "$RESPONSE" | jq -r '.access_token')
 echo "Access Token erhalten: ${ACCESS_TOKEN:0:10}..."
 
-# Speichern für GitHub Actions
+# In GITHUB_ENV speichern
 echo "DROPBOX_ACCESS_TOKEN=$ACCESS_TOKEN" >> $GITHUB_ENV
