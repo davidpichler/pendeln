2bef3139614760fca622f0c9e7133f3afff6d160
David Pichler <pichler.david@hotmail.de>
2025-04-15 22:33:54 +0200

    efficiency

--- codes/travel_time_google_sleeper_old.R
+++ codes/travel_time_google_sleeper_old.R
@@ -33,56 +33,36 @@ time_query <- function(start, ziel) {
   return(res$Time / 60)
 }
 
-# Straßenerkennung via google_directions
-check_road_usage <- function(start, ziel) {
-  route <- google_directions(
-    origin = paste0(start, ", Oberösterreich"),
-    destination = paste0(ziel, ", Oberösterreich"),
-    mode = "driving",
-    departure_time = "now",
-    traffic_model = "best_guess"
-  )
-  
-  steps <- tryCatch(route$routes$legs[[1]]$steps, error = function(e) NULL)
-  if (is.null(steps)) return(c(B127 = NA, B129 = NA))
-  htmls <- sapply(steps, function(x) x$html_instructions)
-  c(
-    B127 = any(grepl("B127", htmls)),
-    B129 = any(grepl("B129", htmls))
-  )
-}
 
-# Funktion für eine komplette Abfragerunde
 run_abfrage <- function() {
   pendler_ergebnis <- list()
   
   for (start in startorte) {
     for (ziel in zielorte) {
-      hin_time <- time_query(start, ziel)
-      retour_time <- time_query(ziel, start)
-      #hin_roads <- check_road_usage(start, ziel)
-      #retour_roads <- check_road_usage(ziel, start)
       
-      abfragezeit <- as.POSIXct(Sys.time(), tz = "Europe/Vienna")
+      vienna_time <- as.POSIXct(Sys.time(), tz = "Europe/Vienna")
+      is_morning <- as.integer(format(vienna_time, "%H")) < 12
+      
+      if (is_morning) {
+        commute_time <- time_query(start, ziel)
+        direction <- "Hin"
+        start_commute <- start
+        ziel_commute <- ziel
+      } else {
+        commute_time <- time_query(ziel, start)
+        direction <- "Retour"
+        start_commute <- ziel
+        ziel_commute <- start
+      }
+      
+      abfragezeit <- vienna_time
       
       pendler_ergebnis[[length(pendler_ergebnis) + 1]] <- data.frame(
         datum_uhrzeit = abfragezeit,
-        richtung = "Hin",
-        startort = start,
-        zielort = ziel,
-        fahrtzeit_minuten = hin_time
-        #b127 = hin_roads["B127"],
-        #b129 = hin_roads["B129"]
-      )
-      
-      pendler_ergebnis[[length(pendler_ergebnis) + 1]] <- data.frame(
-        datum_uhrzeit = abfragezeit,
-        richtung = "Retour",
-        startort = ziel,
-        zielort = start,
-        fahrtzeit_minuten = retour_time
-        #b127 = retour_roads["B127"],
-        #b129 = retour_roads["B129"]
+        richtung = direction,
+        startort = start_commute,
+        zielort = ziel_commute,
+        fahrtzeit_minuten = commute_time
       )
     }
   }
